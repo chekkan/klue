f7ec0e0f0c491dc3c7e7fd91ef5628e69f60f992
Chekkan <harish_babu@outlook.com>
2013-12-30 20:36:17 +0000

    register page now uses FormHelper

--- app/webroot/register.php
+++ app/webroot/register.php
@@ -2,9 +2,10 @@
 
 session_start();
 
+require_once("../lib/Page.php");
 require_once("../../lib/Database.php");
 require_once("../lib/Models/User.php");
-require_once("../lib/Page.php");
+require_once("../../lib/FormHelper.php");
 
 if(isset($_POST['register'])) {
 	// array to save errors
@@ -73,60 +74,15 @@ $page = new Page();
 $page->title = "Register";
 $page->add_css("../css/register.css");
 echo $page->header("Register");
-?>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" role="form">
-	<h2>Register</h2>
-	<?php if (isset($error_messages['main'])) {
-		echo "<p class=\"text-danger\">{$error_messages['main']}</p>";
-	}?>
-	<div class="form-group">
-		<label for="first_name">First Name</label>
-		<input type="text" name="first_name" id="first_name" class="form-control"
-			<?php if(isset($_POST['first_name'])) { echo "value=\"{$_POST['first_name']}\""; } ?>
-		/>
-		<?php if (isset($error_messages['first_name'])) {
-			echo "<p class=\"text-danger\">{$error_messages['first_name']}</p>";
-		}?>
-	</div>
-	<div class="form-group">
-		<label for="last_name">Last Name</label>
-		<input type="text" name="last_name" id="last_name"class="form-control"
-			<?php if(isset($_POST['last_name'])) { echo "value=\"{$_POST['last_name']}\""; } ?>
-		/>
-		<?php if (isset($error_messages['last_name'])) {
-			echo "<p class=\"text-danger\">{$error_messages['last_name']}</p>";
-		}?>
-	</div>
-	<div class="form-group">
-		<label for="email">Email</label>
-		<input type="email" name="email" id="email" class="form-control"
-			<?php if(isset($_POST['email'])) { echo "value=\"{$_POST['email']}\""; } ?>
-		/>
-		<?php if (isset($error_messages['email'])) {
-			echo "<p class=\"text-danger\">{$error_messages['email']}</p>";
-		}?>
-	</div>
-	<div class="form-group">
-		<label for="password">Password</label>
-		<input type="password" name="password" id="password" class="form-control" />
-		<?php if (isset($error_messages['password'])) {
-			echo "<p class=\"text-danger\">{$error_messages['password']}</p>";
-		}?>
-	</div>
-	<div class="form-group">
-		<label for="confirm_password">Confirm Password</label>
-		<input type="password" name="confirm_password" id="confirm_password" class="form-control" />
-	</div>
-	<div class="form-group">
-		<label for="date_of_birth">Date of Birth</label>
-		<input type="date" name="date_of_birth" id="date_of_birth" class="form-control"
-			<?php if(isset($_POST['date_of_birth'])) { echo "value=\"{$_POST['date_of_birth']}\""; } ?>
-		/>
-		<?php if (isset($error_messages['date_of_birth'])) {
-			echo "<p class=\"text-danger\">{$error_messages['date_of_birth']}</p>";
-		}?>
-	</div>
+$form = new FormHelper();
+echo $form->start(array("heading"=>"Register"));
+echo $form->text(array("label"=>"First Name", "name"=>"first_name", "id"=>"FirstName"));
+echo $form->text(array("label"=>"Last Name", "name"=>"last_name", "id"=>"LastName"));
+echo $form->email(array("label"=>"Email", "name"=>"email", "id"=>"Email"));
+echo $form->password(array("label"=>"Password", "name"=>"password", "id"=>"Password"));
+echo $form->date(array("label"=>"Date of Birth", "name"=>"date_of_birth", "id"=>"DateOfBirth"));
+?>
 	<div class="form-group">
 		<input type="submit" name="register" value="Register" class="btn btn-primary" />
 		<span>or <a href="login.php">Login</a></span>
